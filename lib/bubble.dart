fa3135c6b2316fd3a5b6046150748bc8864dda54
Tom Vempala <54186859+tomthomasvempala@users.noreply.github.com>
2020-09-14 14:28:37 +0530

    Initial commit

---
+++ lib/bubble.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class Bubble extends StatefulWidget {
+  @override
+  _BubbleState createState() => _BubbleState();
+}
+
+class _BubbleState extends State<Bubble> {
+  int x1 = -1;
+  int time = 50;
+  bool enabledShuff = true;
+  List<double> listOfValues = [
+    30,
+    60,
+    90,
+    120,
+    150,
+    180,
+    210,
+    240,
+    270,
+    300,
+    330,
+  ];
+  void initState() {
+    listOfValues.shuffle();
+    super.initState();
+  }
+
+  Future<void> bubbleSort() async {
+    for (int i = 0; i < listOfValues.length; i++) {
+      for (x1 = 0; x1 < listOfValues.length - 1 - i; x1++) {
+        await Future.delayed(Duration(milliseconds: time), () {
+          if (listOfValues[x1] > listOfValues[x1 + 1]) {
+            double temp = listOfValues[x1];
+            listOfValues[x1] = listOfValues[x1 + 1];
+            listOfValues[x1 + 1] = temp;
+          }
+          setState(() {});
+        });
+      }
+    }
+    x1 = -1;
+    setState(() {});
+    enabledShuff = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bubble Sort"),
+      ),
+      body: Container(
+          alignment: Alignment.bottomCenter,
+          color: Colors.yellow,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  for (int i = 0; i < listOfValues.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        color: (x1 == i || x1 + 1 == i) && x1 != -1
+                            ? Colors.red
+                            : Colors.blue,
+                        width: 60,
+                        height: listOfValues[i],
+                      ),
+                    )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    OutlineButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        borderSide: BorderSide(width: 5),
+                        onPressed: () async {
+                          enabledShuff = false;
+                          await bubbleSort();
+                        },
+                        child: Text(
+                          "Sort",
+                          style: TextStyle(fontSize: 50),
+                        )),
+                    OutlineButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        borderSide: BorderSide(width: 5),
+                        onPressed: () async {
+                          if (enabledShuff) {
+                            listOfValues.shuffle();
+                            setState(() {});
+                          }
+                        },
+                        child: Text(
+                          "Shuffle",
+                          style: TextStyle(fontSize: 50),
+                        )),
+                  ],
+                ),
+              ),
+              Slider(
+                min: 10,
+                max: 1000,
+                value: time.toDouble(),
+                onChanged: (v) {
+                  time = v.toInt();
+                  setState(() {});
+                },
+              )
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sorting_visualizer/bubble.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sorting Visualizer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Bubble(),
+    );
+  }
+}
